79e1838318adf524109283390de10a41e7b40774
Stephenremmi <stephenremmi@gmail.com>
2024-02-16 15:21:27 +0300

    auth frontend working demo

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { Container, Card, Button } from 'react-bootstrap';
+
+const Hero = () => {
+  return (
+    <div className=' py-5'>
+      <Container className='d-flex justify-content-center'>
+        <Card className='p-5 d-flex flex-column align-items-center hero-card bg-light w-75'>
+          <h1 className='text-center mb-4'>HomeBudget App</h1>
+          <p className='text-center mb-4'>
+            HomeBudget is an expense tracking app designed to help you gain insights into your spending habits and manage your finances effectively.
+          </p>
+          <div className='d-flex'>
+            <Button variant='primary' href='/login' className='me-3'>
+              Sign In
+            </Button>
+            <Button variant='secondary' href='/register'>
+              Register
+            </Button>
+          </div>
+        </Card>
+      </Container>
+    </div>
+  );
+};
+
+export default Hero;
